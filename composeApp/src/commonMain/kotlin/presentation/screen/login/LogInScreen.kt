c8272ffd7e5870dcb64392b7d3aa009d17ad20db
Eduardo Ruesta <eduruesta@hotmail.com>
2024-04-12 11:18:41 -0300

    add google login (#3)

--- composeApp/src/commonMain/kotlin/presentation/screen/login/LogInScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screen/login/LogInScreen.kt
@@ -7,34 +7,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import dev.gitlive.firebase.Firebase
+import com.mmk.kmpauth.firebase.google.GoogleButtonUiContainerFirebase
+import com.mmk.kmpauth.uihelper.google.GoogleSignInButton
 import dev.gitlive.firebase.auth.FirebaseUser
-import dev.gitlive.firebase.auth.auth
-import kotlinx.coroutines.launch
 import presentation.screen.home.HomeScreen
 
 class LogInScreen : Screen {
@@ -42,14 +33,20 @@ class LogInScreen : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
-        val scope = rememberCoroutineScope()
-        val auth = remember { Firebase.auth }
-        val fireBaseUser: FirebaseUser? by remember { mutableStateOf(null) }
-        var userEmail by remember { mutableStateOf("") }
-        var userPassword by remember { mutableStateOf("") }
-        var isError by remember { mutableStateOf(false) }
         var showDialog by remember { mutableStateOf(false) }
+        var signedInUserName: String by remember { mutableStateOf("") }
+        var firebaseUser: FirebaseUser? by remember { mutableStateOf(null) }
+        val onFirebaseResult: (Result<FirebaseUser?>) -> Unit = { result ->
+            if (result.isSuccess) {
+                firebaseUser = result.getOrNull()
+                signedInUserName =
+                    firebaseUser?.displayName ?: firebaseUser?.email ?: "Null User"
+            } else {
+                signedInUserName = "Null User"
+                println("Error Result: ${result.exceptionOrNull()?.message}")
+            }
 
+        }
         if (showDialog) {
             AlertDialog(
                 onDismissRequest = { showDialog = false },
@@ -61,7 +58,7 @@ class LogInScreen : Screen {
                 }
             )
         }
-        if (fireBaseUser == null) {
+        if (firebaseUser != null) {
             Column(
                 modifier = Modifier.fillMaxSize()
                     .padding(16.dp),
@@ -73,8 +70,11 @@ class LogInScreen : Screen {
                     style = MaterialTheme.typography.titleLarge,
                     modifier = Modifier.padding(bottom = 24.dp)
                 )
+                GoogleButtonUiContainerFirebase(onResult = onFirebaseResult) {
+                    GoogleSignInButton(modifier = Modifier.fillMaxWidth()) { this.onClick() }
+                }
 
-                OutlinedTextField(
+/*                OutlinedTextField(
                     value = userEmail,
                     onValueChange = {
                         userEmail = it
@@ -105,9 +105,9 @@ class LogInScreen : Screen {
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(bottom = 16.dp)
-                )
+                )*/
                 Spacer(Modifier.height(12.dp))
-                Button(
+/*                Button(
                     onClick = {
                         if (isError) {
                             showDialog = true
@@ -125,10 +125,10 @@ class LogInScreen : Screen {
                     }
                 ) {
                     Text(text = "Log In")
-                }
+                }*/
             }
         } else {
-            navigator.push(HomeScreen(auth.currentUser))
+            navigator.push(HomeScreen(firebaseUser?.email, firebaseUser?.photoURL))
         }
     }
 
--- composeApp/src/iosMain/kotlin/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/Platform.ios.kt
@@ -1,7 +1,4 @@
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+actual fun onApplicationStartPlatformSpecific() {
 }
-
-actual fun getPlatform(): Platform = IOSPlatform()
